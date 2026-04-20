aa0ba285736518428f40b166cab57b4a99fc915c
gbmelo30-boop <meloguedes30@gmail.com>
2026-04-20 18:50:48 -0300

    Update db.js

--- public/js/db.js
+++ public/js/db.js
@@ -7,7 +7,7 @@ const DB = {
 
     init: async () => {
         const _supabase = supabase.createClient(
-            "https://mrjjbrrypieviykakmby.supabase.co",
+            "https://tnsgxgxpnraqmefldslw.supabase.co",
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yampicnJ5cGlldml5a2FrbWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MTU4OTIsImV4cCI6MjA4NTI5MTg5Mn0.mldEk-7-vU0BdwlNf5COG_Dj9sRnFWrpQMkqTAZNw_8"
         );
         window.supabaseClient = _supabase;
